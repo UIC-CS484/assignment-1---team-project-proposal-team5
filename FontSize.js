3c3898b0b21b98d110fefd52f264cf81f7efe3ad
Aryan Thaman <aryan.thaman.us@gmail.com>
2021-09-24 17:02:58 -0500

    Added JS Logic.

---
+++ FontSize.js
@@ -0,0 +1,12 @@
+function FontSizeS() {
+    const containerID = document.getElementById('container');
+    containerID.className = "smallText";
+}
+function FontSizeM() {
+    const containerID = document.getElementById('container');
+    containerID.className = "mediumText";
+}
+function FontSizeL() {
+    const containerID = document.getElementById('container');
+    containerID.className = "largeText";
+}
